60b47fa25c6d7e42248db7c63be5a8f6f776b6f6
Priu <jmpriu@gmail.com>
2021-12-03 20:25:51 +0100

    Add day 2 solution

---
+++ 2/index.js
@@ -0,0 +1,78 @@
+const { readStringArrayFromFile } = require("../common");
+
+const INPUT = readStringArrayFromFile("./input.txt");
+
+console.log(INPUT);
+
+const executeCommands = (commandList) => {
+    let position = 0;
+    let depth = 0;
+    commandList
+        .map(command => command
+        .split(' '))
+        .map(([action, number]) => [action, Number(number)])
+        // "command X" -> ["command", X]
+        .forEach(([action, number]) => {
+            switch(action) {
+                case "forward":
+                    position = position + number;
+                    break;
+                case "down":
+                    depth = depth + number;
+                    break;
+                case "up":
+                    depth = depth - number;
+                    break;
+                default:
+                    console.log("Can't execute", command, number);
+            }
+        });
+    return { position, depth };
+}
+
+const executeCommandsPart2 = (commandList) => {
+    let position = 0;
+    let depth = 0;
+    let aim = 0;
+    commandList
+        .map(command => command
+        .split(' '))
+        .map(([action, number]) => [action, Number(number)])
+        // "command X" -> ["command", X]
+        .forEach(([action, number]) => {
+            switch(action) {
+                case "forward":
+                    position = position + number;
+                    depth = depth + aim * number;
+                    break;
+                case "down":
+                    aim = aim + number;
+                    break;
+                case "up":
+                    aim = aim - number;
+                    break;
+                default:
+                    console.log("Can't execute", command, number);
+            }
+        });
+    return { position, depth };
+}
+
+const { position, depth } = executeCommands(INPUT);
+
+console.log(`
+    PART 1
+    position:${position}
+    depth: ${depth}
+    multiplication: ${position * depth}
+`)
+
+const { position: postionPart2, depth:depthPart2 } = executeCommandsPart2(INPUT);
+
+console.log(`
+    PART 2
+    position:${postionPart2}
+    depth: ${depthPart2}
+    multiplication: ${postionPart2 * depthPart2}
+`)
+
