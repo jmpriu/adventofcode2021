69682dd0884d76c53fcb61b3d6b2389dddaa0af9
Priu <jmpriu@gmail.com>
2021-12-03 20:25:17 +0100

    Improve read file utils

--- common/index.js
+++ common/index.js
@@ -1,16 +1,26 @@
 const fs = require('fs');
 
-const NEWLINE = `
-`;
+const WINDOWS_NEWLINE = '\r\n';
+const UNIX_NEWLINE = '\n';
 
 const readFile = (filename) => fs.readFileSync(filename, { encoding: "utf-8" });
 
+const splitStringWithNewLine = (text) => {
+    if (text.indexOf(WINDOWS_NEWLINE >= 0)) {
+        return text.split(WINDOWS_NEWLINE);
+    }
+    return text.split(UNIX_NEWLINE);
+}
+
 const readNumberArrayFromFile = (filename) => 
-    readFile(filename)
-    .split(NEWLINE)
+    splitStringWithNewLine(readFile(filename))
     .map(Number);
+
+const readStringArrayFromFile = (filename) =>
+    splitStringWithNewLine(readFile(filename));
 
 module.exports = {
     readFile,
-    readNumberArrayFromFile
+    readNumberArrayFromFile,
+    readStringArrayFromFile
 }
