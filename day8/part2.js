469e7750a447ee27d88817245789893ea28a63f4
Priu <jmpriu@gmail.com>
2021-12-08 14:32:19 +0100

    Add sevent-segment display comment to day 8

--- day8/part2.js
+++ day8/part2.js
@@ -4,6 +4,25 @@ const INPUT = readStringArrayFromFile("/day8/input.txt");
 
 const formatSetAsString = (set) => set.sort().join('');
 
+/*
+  0:      1:      2:      3:      4:
+ aaaa    ....    aaaa    aaaa    ....
+b    c  .    c  .    c  .    c  b    c
+b    c  .    c  .    c  .    c  b    c
+ ....    ....    dddd    dddd    dddd
+e    f  .    f  e    .  .    f  .    f
+e    f  .    f  e    .  .    f  .    f
+ gggg    ....    gggg    gggg    ....
+
+  5:      6:      7:      8:      9:
+ aaaa    aaaa    aaaa    aaaa    aaaa
+b    .  b    .  .    c  b    c  b    c
+b    .  b    .  .    c  b    c  b    c
+ dddd    dddd    ....    dddd    dddd
+.    f  e    f  .    f  e    f  .    f
+.    f  e    f  .    f  e    f  .    f
+ gggg    gggg    ....    gggg    gggg
+*/
 const getUSPAndDigits = (inputLine) => {
     let [USP, digitsOutput] = inputLine.split(' | ');
     USP = USP.split(' ').map(pattern => pattern.split(''));
